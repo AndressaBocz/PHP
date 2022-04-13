5bec21846532e0d92c68ee556fa6a9f8b08e2bd6
Andressa Bocz <102601275+AndressaBocz@users.noreply.github.com>
2022-04-13 13:15:15 -0300

    Delete CadastroPHP/programaphp directory

--- CadastroPHP/programaphp/footer.php
+++
@@ -1,9 +0,0 @@
-<hr><hr>
-    <footer>
-         <p><b>Nosso endereço é:</b>
-         </address>Av. Francisco Rodrigues filho, 1233- Mogilar -
-         </p>Todos os direitos reservados- Professor: João Vellardi
-        </footer>
-
-     </body>
-</html>
